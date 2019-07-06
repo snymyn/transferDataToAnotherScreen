cac5a2adab2d6fa866e363548963c93ef7401a99
SHINYA <snymyn@yahoo.com>
2019-07-06 17:07:54 +0900

    first commit

---
+++ TransferDataToAnotherScreen/TransferDataToAnotherScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TransferDataToAnotherScreen
+//
+//  Created by aa372953 on 2019/07/06.
+//  Copyright © 2019 snymyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
